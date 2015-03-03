47ba9a4bbb985d198e90d4f7f5f36397b5626509
Randall McPherson <rlmcpherson@gmail.com>
2015-03-02 22:28:55 -0500

    simplified secred handshake

--- secret-handshake/secret.go
+++ secret-handshake/secret.go
@@ -1,16 +1,5 @@
 package secret
 
-import "encoding/binary"
-
-const (
-	_ byte = 1 << iota
-	wink
-	doubleBlink
-	closeEyes
-	jump
-	reverse
-)
-
 func Handshake(code int) []string {
 
 	var h []string
@@ -18,24 +7,19 @@ func Handshake(code int) []string {
 		return h
 	}
 
-	b := make([]byte, 1)
-	binary.PutVarint(b, int64(code))
-
-	mask := b[0]
-
-	if wink&mask != 0 {
+	if 1&code != 0 {
 		h = append(h, "wink")
 	}
-	if doubleBlink&mask != 0 {
+	if 2&code != 0 {
 		h = append(h, "double blink")
 	}
-	if closeEyes&mask != 0 {
+	if 4&code != 0 {
 		h = append(h, "close your eyes")
 	}
-	if jump&mask != 0 {
+	if 8&code != 0 {
 		h = append(h, "jump")
 	}
-	if reverse&mask != 0 {
+	if 16&code != 0 {
 		for i, j := 0, len(h)-1; i < j; i, j = i+1, j-1 {
 			h[i], h[j] = h[j], h[i]
 		}
